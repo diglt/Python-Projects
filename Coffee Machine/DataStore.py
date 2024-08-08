0a5030650f45b97ca0acb2ccf9883892ec422281
digit <125754881+diglt@users.noreply.github.com>
2024-08-08 20:36:22 +0100

    Create DataStore.py

---
+++ Coffee Machine/DataStore.py
@@ -0,0 +1,34 @@
+Coffees = {
+    "espresso": {
+        "Price": 1.50,
+        "Water": 50,
+        "Coffee": 18,
+        "Milk": 0
+    },
+    "latte": {
+        "Price": 2.50,
+        "Water": 200,
+        "Coffe": 24,
+        "Milk": 150
+    },
+    "cappuccino": {
+        "Price": 3,
+        "Water": 250,
+        "Coffee": 24,
+        "Milk": 100
+    }
+}
+
+StartingResources = {
+    "Water": 300,
+    "Milk": 200,
+    "Coffee": 100,
+    "Money": 0
+}
+
+Currency = {
+    "Quater": 0.25,
+    "Dime": 0.10,
+    "Nickel": 0.05,
+    "Penny": 0.01
+}
