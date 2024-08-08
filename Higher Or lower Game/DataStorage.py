7cb7a769ee3289bc1e66477077baa8274b9fa0d1
digit <125754881+diglt@users.noreply.github.com>
2024-08-08 18:53:42 +0100

    Create DataStorage.py

---
+++ Higher Or lower Game/DataStorage.py
@@ -0,0 +1,126 @@
+logo = """""       
+  ___ ___ .__       .__                  
+ /   |   \|__| ____ |  |__   ___________ 
+/    ~    \  |/ ___\|  |  \_/ __ \_  __ \/
+\    Y    /  / /_/  >   Y  \  ___/|  | \/
+ \___|_  /|__\___  /|___|  /\___  >__|   
+       \/   /_____/      \/     \/       
+.____                                    
+|    |    ______  _  __ ___________      
+|    |   /  _ \ \/ \/ // __ \_  __ \     
+|    |__(  <_> )     /\  ___/|  | \/     
+|_______ \____/ \/\_/  \___  >__|        
+        \/                 \/     
+"""
+
+VS = """
+____   _____________
+\   \ /   /   _____/
+ \   Y   /\_____  \ 
+  \     / /        \/
+   \___/ /_______  /
+                 \/
+"""
+
+data = {
+    "Cristiano Ronaldo": {
+        "followers": "606M",  # As of recent data
+        "known_for": "Football/Soccer",
+        "birth_country": "Portugal"
+    },
+    "Lionel Messi": {
+        "followers": "486M",
+        "known_for": "Football/Soccer",
+        "birth_country": "Argentina"
+    },
+    "Selena Gomez": {
+        "followers": "430M",
+        "known_for": "Singer/Actress",
+        "birth_country": "United States"
+    },
+    "Kylie Jenner": {
+        "followers": "400M",
+        "known_for": "Reality TV/Businesswoman",
+        "birth_country": "United States"
+    },
+    "Dwayne 'The Rock' Johnson": {
+        "followers": "393M",
+        "known_for": "Actor/Wrestler",
+        "birth_country": "United States"
+    },
+    "Ariana Grande": {
+        "followers": "391M",
+        "known_for": "Singer/Actress",
+        "birth_country": "United States"
+    },
+    "Kim Kardashian": {
+        "followers": "379M",
+        "known_for": "Reality TV/Businesswoman",
+        "birth_country": "United States"
+    },
+    "Beyoncé": {
+        "followers": "318M",
+        "known_for": "Singer/Actress",
+        "birth_country": "United States"
+    },
+    "Khloé Kardashian": {
+        "followers": "311M",
+        "known_for": "Reality TV/Businesswoman",
+        "birth_country": "United States"
+    },
+    "Justin Bieber": {
+        "followers": "288M",
+        "known_for": "Singer",
+        "birth_country": "Canada"
+    },
+    "Kendall Jenner": {
+        "followers": "295M",
+        "known_for": "Model/Reality TV",
+        "birth_country": "United States"
+    },
+    "Nike": {
+        "followers": "309M",
+        "known_for": "Sportswear Brand",
+        "birth_country": "United States"
+    },
+    "National Geographic": {
+        "followers": "285M",
+        "known_for": "Media/Photography",
+        "birth_country": "United States"
+    },
+    "Virat Kohli": {
+        "followers": "258M",
+        "known_for": "Cricket",
+        "birth_country": "India"
+    },
+    "Taylor Swift": {
+        "followers": "272M",
+        "known_for": "Singer/Songwriter",
+        "birth_country": "United States"
+    },
+    "Jennifer Lopez": {
+        "followers": "258M",
+        "known_for": "Singer/Actress",
+        "birth_country": "United States"
+    },
+    "Neymar Jr.": {
+        "followers": "224M",
+        "known_for": "Football/Soccer",
+        "birth_country": "Brazil"
+    },
+    "Cardi B": {
+        "followers": "173M",
+        "known_for": "Rapper",
+        "birth_country": "United States"
+    },
+    "Kourtney Kardashian": {
+        "followers": "233M",
+        "known_for": "Reality TV/Businesswoman",
+        "birth_country": "United States"
+    },
+    "Zendaya": {
+        "followers": "196M",
+        "known_for": "Actress/Singer",
+        "birth_country": "United States"
+    }
+}
