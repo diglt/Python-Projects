464455fb5dac0d7d445ddc5dc489c02e2e408758
digit <125754881+diglt@users.noreply.github.com>
2024-09-08 19:56:48 +0100

    Create encrypt4r.py

---
+++ encrypt4r.py
@@ -0,0 +1,203 @@
+# ok yes, i know i normally do modules for my more recent projects. However, it's alot less hassle to spread this script if all the libraries and functions are already preinstalled in python
+# so yes ok good i do stuf ahkdwlaldlasldl
+
+title = """                                                                                                    
+\x1b[31m                                                                                         ,d8                
+                                                                            ,d         ,d888                
+                                                                            88       ,d8" 88                
+ ,adPPYba,  8b,dPPYba,    ,adPPYba,  8b       d8  8b,dPPYba,  8b,dPPYba,  MM88MMM  ,d8"   88    8b,dPPYba,  
+a8P_____88  88P'   `"8a  a8"     ""  `8b     d8'  88P'   "Y8  88P'    "8a   88   ,d8"     88    88P'   "Y8  
+8PP"""""""  88       88  8b           `8b   d8'   88          88       d8   88   8888888888888  88          
+"8b,   ,aa  88       88  "8a,   ,aa    `8b,d8'    88          88b,   ,a8"   88,           88    88          
+ `"Ybbd8"'  88       88   `"Ybbd8"'      Y88'     88          88`YbbdP"'    "Y888         88    88          
+                                         d8'                  88                                            
+                                        d8'                   88                                            
+"""
+
+import base64
+import random
+
+
+Methods = ["Caesar Cipher", "Base 64"]
+Choices = ["Encryption", "Decryption"]
+Characters = []
+Numbers = []
+Special_Characters = []
+Encoded_Table = []
+Decoded_Table = []
+
+Characters.extend("abcdefghijklmnopqrstuvwxyz")
+Numbers.extend("1234567890")
+Special_Characters.extend("¬!£$%^*()_+-`[]{}'#/.,\\")
+
+Continue_Running = True
+My_Method = None
+My_Choice = None
+User_String = None
+
+
+def AddToString(letter, choice, shift_number):
+    global Encoded_String
+
+    if choice:
+        new_index = Characters.index(letter) + shift_number
+
+        if new_index > len(Characters) - 1:
+            new_index = new_index - len(Characters)
+            new_letter = Characters[new_index]
+
+            Encoded_Table.append(new_letter)
+        else:
+            Encoded_Table.append(Characters[new_index])
+
+    else:
+        new_index = Characters.index(letter.lower()) + shift_number
+
+        if new_index > len(Characters) - 1:
+            new_index = new_index - len(Characters)
+            new_letter = Characters[new_index].upper()
+
+            Encoded_Table.append(new_letter)
+        else:
+            Encoded_Table.append(Characters[new_index].upper())
+
+
+
+
+def EncryptCeaserCypher(String):
+    Encoded_String = ""
+    Shift_Amount = random.randint(1, len(Characters))
+
+
+    for letter in String:
+        if letter == " ":
+            Encoded_Table.append(letter)
+
+        elif letter in Characters:
+            AddToString(letter, True, Shift_Amount)
+
+        elif letter.isupper():
+            AddToString(letter, False, Shift_Amount)
+
+    for character in Encoded_Table:
+        Encoded_String += character
+
+    if len(Encoded_String) > 0:
+        print(f"\nHere's your encoded result: {Encoded_String}, your shift is {Shift_Amount}")
+
+
+
+def DecryptCeaserCypher(String):
+    Shift = int(input("\nPlease enter the shift number given to you: "))
+    Decoded_String = ""
+
+    for letter in String:
+        if letter == " ":
+            Decoded_String += letter
+
+        elif letter in Characters:
+            new_index = Characters.index(letter) - Shift
+            
+            if new_index > len(Characters) - 1:
+                new_index = new_index - len(Characters)
+                new_letter = Characters[new_index]
+
+                Decoded_String += new_letter
+            else:
+                Decoded_String += Characters[new_index]
+
+        elif letter.isupper():
+            new_index = Characters.index(letter.lower()) - Shift
+
+            if new_index > len(Characters) - 1:
+                new_index = new_index - len(Characters)
+                new_letter = Characters[new_index].upper()
+
+                Decoded_String += new_letter
+            else:
+                Decoded_String += Characters[new_index].upper()
+
+    if len(Decoded_String) > 0:
+        print(f"\nHere's your decoded result: {Decoded_String}")
+
+
+
+while Continue_Running:
+    index = 0
+
+    print(title)
+    print("Choose Your Encryption Method.")
+
+    def DecideEncryption(Method, Choice, String):
+        Method = Methods[Method]
+        Choice = Choices[Choice]
+
+        Encode_Step_One = String.encode('utf-8')
+
+        if Choice == "Encryption":
+            if Method == "Caesar Cipher":
+                EncryptCeaserCypher(String)      
+
+
+            if Method == "Base 64":
+                encoded_byte = Encode_Step_One
+                encoded_bytes = base64.b64encode(encoded_byte)
+                encoded_string = encoded_bytes.decode('utf-8')
+
+                print(f"\nHere's your encoded result: {encoded_string}")
+        else:
+            if Method == "Caesar Cipher":
+                DecryptCeaserCypher(String)
+
+            if Method == "Base 64":
+                encoded_byte = Encode_Step_One
+                encoded_bytes = base64.b64decode(encoded_byte)
+                encoded_string = encoded_bytes.decode('utf-8')
+
+                print(f"\nHere's your decoded result: {encoded_string}")
+
+
+
+    def ChooseMethod():
+        global My_Method
+        global index
+
+        for Method in Methods:
+            index += 1
+            new_index = str(index) + ". "
+            print(f"\x1b[31m{new_index}{Method}")
+
+        index = 0
+        My_Method = int(input("\n")) - 1
+
+
+    def ChooseType():
+        global My_Choice
+        global index
+
+        print("\nChoose your cryption type!")
+
+        for Choice in Choices:
+            index += 1
+            new_index = str(index) + ". "
+
+            print(new_index, Choice)
+
+        My_Choice = int(input("\n")) - 1
+
+    ChooseMethod()
+    ChooseType()
+
+    User_String = str(input("\nInput ur text here:\n"))
+
+    DecideEncryption(My_Method, My_Choice, User_String)
+
+    Continue = str(input("Would you like to continue? Type 'Y' or 'N'. ")).lower()
+
+    if Continue[0] == "y":
+        for a in range(1, 100):
+            print("\n")
+    else:
+        Continue_Running = False
+        break
+
