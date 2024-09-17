500b9af13eb5b5c377c6c2d4deecf43bd0fa6033
digit <125754881+diglt@users.noreply.github.com>
2024-09-17 20:35:10 +0100

    Create Auto_Typer_Monkey_Type.py

---
+++ Fun Projects/Auto_Typer_Monkey_Type.py
@@ -0,0 +1,34 @@
+typing_speed = 0.000001
+
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pyautogui
+import time
+
+driver = webdriver.Edge()
+
+driver.get("https://monkeytype.com")
+
+time.sleep(5)  
+
+need_to_run = True
+
+while need_to_run:
+    html_content = driver.page_source
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    for div in soup.find_all('div', class_='word active'):
+        print("Found div:", div)
+        word = ""
+        
+        # Extract and index the letters inside this div
+        for letter in div.get_text(strip=True):
+            word += letter
+        
+        word += " "
+        
+        pyautogui.write(word, interval=0.000001)
+
+        word = ""
